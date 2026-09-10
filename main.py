9f42a077a57ca9bf989e0507b50254a5d4cbf785
ANU SREE <anusreesatheesh217@gmail.com>
2026-09-10 17:34:17 +0530

    python tasks

---
+++ main.py
@@ -0,0 +1,47 @@
+# A.Python is an indent based programming language
+teams = ['Data', 'AI', 'DevOps']
+for t in teams:
+     print('Hello', t, 'Team from Inceptez Technologies')
+     print('Keep Learning and Exploring!')
+
+# B. Commented line in Python
+
+# Number of students
+students = 100
+
+# Number of trainers
+trainers = 2
+
+'''
+This program calculates the total number
+of students and trainers.
+'''
+
+total = students + trainers
+print(total)
+
+# Dead Code
+# print("Welcome to Inceptez Python Learning")
+
+# Re-activated Code
+print("Welcome to Inceptez Python Learning")
+
+
+# C. Playing with Quotes
+
+msg1 = "This is Inceptez's \"Python\" class for Data Engineers & AI Engineers"
+print(msg1)
+
+msg2 = 'This is Inceptez\'s "Python" class for Data Engineers & AI Engineers'
+print(msg2)
+
+msg3 = '''This is Inceptez's "Python" class for Data Engineers & AI Engineers'''
+print(msg3)
+
+message = '''
+Welcome to Inceptez Technologies!
+Python Training: Basics
+Enjoy your learning journey.
+'''
+
+print(message)
